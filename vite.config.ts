c37c1f62e1927ba90c9aa6c51d7d27cb9ebf7631
siggiux <sigurdur.gudmundsson@hyperisland.se>
2026-05-26 12:30:06 +0000

    add vite base path and github actions deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 // Minimal Vite config. We use React's plugin and nothing else.
 // If you ever want path aliases, add `resolve.alias` here.
 export default defineConfig({
-  plugins: [react()],
+ base: '/ux-glossary/', 
+ plugins: [react()],
 });
